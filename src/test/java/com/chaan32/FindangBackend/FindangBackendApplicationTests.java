26a2ca16d0ba893c53d727d0898c88ff9673e50e
chaan32 <haechan8864@naver.com>
2025-02-16 18:43:38 +0900

    [Setting]

---
+++ src/test/java/com/chaan32/FindangBackend/FindangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaan32.FindangBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
