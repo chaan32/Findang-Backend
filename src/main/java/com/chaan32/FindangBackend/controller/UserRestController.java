ede494bd71221d54eba3f6c537660760b3d092a7
chaan32 <haechan8864@naver.com>
2025-03-14 20:09:51 +0900

    컨트롤러 반환값 Json 형태로 변경하고 PostMapping으로 바꿈 - Login

--- src/main/java/com/chaan32/FindangBackend/controller/UserRestController.java
+++ src/main/java/com/chaan32/FindangBackend/controller/UserRestController.java
@@ -5,8 +5,12 @@ import com.chaan32.FindangBackend.dto.UserDTO;
 import com.chaan32.FindangBackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -25,19 +29,35 @@ public class UserRestController {
     }
 
     // 로그인 아이디 중복 체크
-    @GetMapping("/check/login-id")
+    @PostMapping("/check/login-id")
     public Boolean checkLoginId(@RequestBody UserDTO requestDTO){
         log.info("[ [UserRestController] / [checkLoginId] ] 입력된 user_login_id  : {}", requestDTO.getUserLoginId());
         return !userService.overlapCheckLoginId(requestDTO.getUserLoginId());
     }
 
 
+
     // 로그인
-    @GetMapping("/login")
-    public Boolean login(@RequestBody UserDTO userLoginDTO){
+//    @GetMapping("/login")
+//    public Boolean login(@RequestBody UserDTO userLoginDTO){
+//        log.info("[ [UserRestController] / [login] ] 입력된 userLoginDTO : {}", userLoginDTO);
+//        Boolean b = userService.loginProcess(userLoginDTO);
+//        log.info("[ [UserRestController] / [login] ] 로그인 결과 : {}", b);
+//        return b;
+//    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody UserDTO userLoginDTO){
         log.info("[ [UserRestController] / [login] ] 입력된 userLoginDTO : {}", userLoginDTO);
-        return userService.loginProcess(userLoginDTO);
+        Boolean b = userService.loginProcess(userLoginDTO);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", b);
+
+        log.info("[ [UserRestController] / [login] ] 로그인 결과 : {}", b);
+        return ResponseEntity.ok(response);
     }
+
 
     // 유저 이름 조회
     @GetMapping("/name")
